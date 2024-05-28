d81fc5d97eaf5a0615b78a95b91f2bbfe1d96073
Max Kung <maxkung06@gmail.com>
2024-05-28 19:22:19 -0400

    Update main.py
    Added option to change the voice between male and female

--- loop.py
+++ loop.py
@@ -2,11 +2,26 @@
 import time, random
 from subprocess import call
 
+# Memory function
+def getVoice():
+    voices_app = open("settings.csv", "r")
+    data = []
+    data_line = voices_app.readline().split(',')
+    data.append(data_line)
+    voices_app.close()
+    return data[0][0]
+
+# Set voice
+try:
+    theVoice = getVoice()
+except:
+    theVoice = "female2"
+
 # Function
 def randomLine():
     outputs = ["air", "land", "sea"]
     x = random.randint(0, len(outputs)-1)
-    call("espeak \"" + outputs[x] + "\"", shell=True)
+    call("espeak -v " + theVoice + " \"" + outputs[x] + "\"", shell=True)
 
 # Run
 while True:
--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 from subprocess import call, Popen, PIPE
 from vosk import Model, KaldiRecognizer
-import pyaudio
+import pyaudio, time
+
+# Memory function
+def getVoice():
+    voices_app = open("settings.csv", "r")
+    data = []
+    data_line = voices_app.readline().split(',')
+    data.append(data_line)
+    voices_app.close()
+    return data[0][0]
+
+# Check if memory file exists
+defaultVoice = "female2"
+try:
+    currentVoice = getVoice()
+except:
+    currentVoice = defaultVoice
+    f = open("settings.csv", "w")
+    f.write(defaultVoice)
+    f.close()
 
 # Load the Vosk model
 model = Model("path_to_your_vosk_model_directory")
@@ -18,18 +37,19 @@ stream.start_stream()
 gameRunning = False
 startCommand = "start"
 endGameCommand = "stop"
+changeVoiceCommand = "voice"
 
 # Define the command to run the process
 command = ['python', 'loop.py']
 
 # Startup sound
-call("espeak \"Hello\"", shell=True)
+call("espeak -v " + currentVoice + " \"Welcome\"", shell=True)
 
 # Run
 transcript = []
 while True:
-    # Process audio chunks as they come in
-    data = stream.read(4096)
+    # Process audio chunks as they come in.
+    data = stream.read(1000)
     if recognizer.AcceptWaveform(data):
         transcript = transcript + recognizer.Result()[14:-3].split()
     else:
@@ -44,6 +64,26 @@ while True:
             gameRunning = False
             try:
                 proc.kill()
+            except NameError:
+                pass
+            break
+        elif x == changeVoiceCommand and gameRunning == False:
+            if currentVoice == defaultVoice:
+                currentVoice = "male2"
+                call("espeak -v " + currentVoice + " \"male\"", shell=True)
+            else:
+                currentVoice = defaultVoice
+                call("espeak -v " + currentVoice + " \"female\"", shell=True)
+            fh = open("settings.csv", "w")
+            fh.write(currentVoice)
+            fh.close()
+            break
+        elif x == changeVoiceCommand and gameRunning == True:
+            try:
+                proc.kill()
+                call("espeak -v " + currentVoice + " \"Cannot do that yet\"", shell=True)
+                time.sleep(4)
+                proc = Popen(command, stdout=PIPE, stderr=PIPE)
             except NameError:
                 pass
             break
