58d9d4ace1c5ddd7fa020661ba062912e5a9ce85
Max Kung <maxkung06@gmail.com>
2025-03-05 16:31:30 -0500

    update main.py
    Major improvements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
-from subprocess import call, Popen, PIPE
+#!/usr/bin/env python
+from subprocess import call
 from vosk import Model, KaldiRecognizer
-import pyaudio, time
+import pyaudio, threading, time, random
 
 # Memory function
 def getVoice():
@@ -22,15 +22,18 @@ except:
     f.write(defaultVoice)
     f.close()
 
-# Load the Vosk model
-model = Model("path_to_your_vosk_model_directory")
-
-# Initialize the recognizer with the model
+# Model loading and setup (replace with your own model path)
+model_path = "vosk-model-small-en-us-0.15"
+model = Model(model_path)
 recognizer = KaldiRecognizer(model, 16000)
+
+# Audio stream setup
+samplerate = 16000
+blocksize = 8192
 
 # Start the microphone and process audio input
 p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=samplerate, input=True, frames_per_buffer=blocksize)
 stream.start_stream()
 
 # Initiate variables
@@ -38,56 +41,78 @@ gameRunning = False
 startCommand = "start"
 endGameCommand = "stop"
 changeVoiceCommand = "voice"
-
-# Define the command to run the process
-command = ['python', 'loop.py']
+transcript = []
 
 # Startup sound
 call("espeak -v " + currentVoice + " \"Welcome\"", shell=True)
 
-# Run
-transcript = []
-while True:
-    # Process audio chunks as they come in.
-    data = stream.read(1000)
-    if recognizer.AcceptWaveform(data):
-        transcript = transcript + recognizer.Result()[14:-3].split()
-    else:
-        transcript = transcript + recognizer.PartialResult()[14:-3].split()
-    # Here, you can parse the result and execute commands based on the recognized text
-    for x in transcript:
-        if x == startCommand and gameRunning == False:
-            gameRunning = True
-            proc = Popen(command, stdout=PIPE, stderr=PIPE)
-            break
-        elif x == endGameCommand and gameRunning == True:
-            gameRunning = False
-            try:
-                proc.kill()
-            except NameError:
-                pass
-            break
-        elif x == changeVoiceCommand and gameRunning == False:
-            if currentVoice == defaultVoice:
-                currentVoice = "male2"
-                call("espeak -v " + currentVoice + " \"male\"", shell=True)
-            else:
-                currentVoice = defaultVoice
-                call("espeak -v " + currentVoice + " \"female\"", shell=True)
-            fh = open("settings.csv", "w")
-            fh.write(currentVoice)
-            fh.close()
-            break
-        elif x == changeVoiceCommand and gameRunning == True:
-            try:
-                proc.kill()
-                call("espeak -v " + currentVoice + " \"Cannot do that yet\"", shell=True)
-                time.sleep(4)
-                proc = Popen(command, stdout=PIPE, stderr=PIPE)
-            except NameError:
-                pass
-            break
+# Function 1
+def randomLine():
+    while True:
+        if gameRunning:
+            outputs = ["air", "land", "sea"]
+            x = random.randint(0, len(outputs)-1)
+            call("espeak -v " + currentVoice + " \"" + outputs[x] + "\"", shell=True)
+            time.sleep(4)
         else:
             pass
-    for i in transcript:
-        transcript.remove(i)
+
+# Function 2
+def transcribeCommand():
+    global gameRunning, currentVoice, transcript
+    while True:
+        # Process audio chunks as they come in.
+        data = stream.read(4096)
+        if recognizer.AcceptWaveform(data):
+            transcript = transcript + recognizer.Result()[14:-3].split()
+        else:
+            transcript = transcript + recognizer.PartialResult()[14:-3].split()
+        # Here, you can parse the result and execute commands based on the recognized text
+        for x in transcript:
+            if x == startCommand and gameRunning == False:
+                gameRunning = True
+                break
+            elif x == endGameCommand and gameRunning == True:
+                gameRunning = False
+                break
+            elif x == changeVoiceCommand and gameRunning == False:
+                for i in transcript:
+                    transcript.remove(i)
+                if currentVoice == defaultVoice:
+                    currentVoice = "male2"
+                    call("espeak -v " + currentVoice + " \"male\"", shell=True)
+                else:
+                    currentVoice = defaultVoice
+                    call("espeak -v " + currentVoice + " \"female\"", shell=True)
+                fh = open("settings.csv", "w")
+                fh.write(currentVoice)
+                fh.close()
+                break
+            elif x == changeVoiceCommand and gameRunning == True:
+                for i in transcript:
+                    transcript.remove(i)
+                gameRunning = False
+                call("espeak -v " + currentVoice + " \"Cannot do that yet\"", shell=True)
+                time.sleep(2)
+                gameRunning = True
+                break
+            else:
+                pass
+        for i in transcript:
+            transcript.remove(i)
+
+#Run
+if __name__ == "__main__":
+    threads = []
+    f1 = threading.Thread(target=randomLine)
+    f2 = threading.Thread(target=transcribeCommand)
+    threads.append(f1)
+    f1.start()
+    threads.append(f2)
+    f2.start()
+
+    for t in threads:
+        t.join()
+
+    stream.stop_stream()
+    stream.close()
